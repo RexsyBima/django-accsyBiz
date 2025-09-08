1b0200ea4c30b7eb9693431ba40602faffa16924
rexsybimatw <rexsy.bimq12@gmail.com>
2025-09-08 13:47:15 +0700

    updated context data

--- a_pages/views.py
+++ a_pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from typing import Any
 from django.views.generic import TemplateView
+from a_places.models import Place
 
 # Create your views here.
 
@@ -10,3 +12,9 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = 'a_pages/home.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['places'] = list(Place.objects.values('latitude', 'longitude', 'name', 'category'))
+        print(context['places'])    
+        return context
