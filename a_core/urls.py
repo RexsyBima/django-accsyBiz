c1a119dc81ae049fb7479bc45a9f1605d891d1f1
rexsybimatw <rexsy.bimq12@gmail.com>
2025-09-08 10:10:42 +0700

    added home.html
    the base.html is used to extended there

--- a_core/urls.py
+++ a_core/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='base.html'), name='home'),
+    path('', TemplateView.as_view(template_name='a_core/home.html'), name='home'),
 ]
