b7f4135d4d53e39fd56ac6e79655e0e3942d1f05
rexsybimatw <rexsy.bimq12@gmail.com>
2025-09-08 14:35:52 +0700

    fix urlpatterns

--- a_core/urls.py
+++ a_core/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('a_pages.urls'))
+    path('place/', include('a_places.urls')),
+    path('', include('a_pages.urls')),
 ]
