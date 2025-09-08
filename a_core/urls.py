cdd3200a8cd59fb4410f3f923c9fb137439af6bc
rexsybimatw <rexsy.bimq12@gmail.com>
2025-09-08 10:37:59 +0700

    better handling include statement

--- a_core/urls.py
+++ a_core/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='a_core/home.html'), name='home'),
+    path('', include('a_pages.urls'))
 ]
