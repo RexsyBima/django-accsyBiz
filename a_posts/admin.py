c3465b91b76cd0d7308ef84fe7ab30629d56b76c
rexsybimatw <rexsy.bimq12@gmail.com>
2025-09-09 18:55:34 +0700

    added a_posts app

---
+++ a_posts/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import PostFeature
+
+# Register your models here.
+admin.site.register(PostFeature)
---
+++ a_posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from a_features.models import Feature
+from a_places.models import Place
+from django.contrib.auth.models import User
+
+# Create your models here.
+class PostFeature(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='post_features')
+    feature = models.ForeignKey(Feature, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('place', 'feature', 'user')
+
+    def __str__(self):
+        return f"{self.place.name} - {self.feature.code} by {self.user.username}"
+
+
